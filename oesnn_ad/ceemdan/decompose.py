42278051e9945d5bcdbf47d2189a44161fdde878
Mariusz Paluch <mariuszpaluch001@gmail.com>
2023-12-25 22:02:27 +0100

    Added ceemdan decompose

---
+++ oesnn_ad/ceemdan/decompose.py
@@ -0,0 +1,39 @@
+from threading import Thread
+
+import numpy as np
+import numpy.typing as npt
+from PyEMD import CEEMDAN
+
+from oesnn_ad import OeSNNAD
+
+
+class Decompose:
+    def __init__(self, stream: npt.NDArray[np.float64],
+                 trials=100,
+                 range_threshold=1e-3,
+                 total_power_threshold=1e-2) -> None:
+        self.stream = stream
+        self.ceemdan = CEEMDAN(trials=trials, range_threshold=range_threshold,
+                               total_power_threshold=total_power_threshold)
+
+    def _decompose(self, channels: int):
+        return self.ceemdan.ceemdan(self.stream, max_imf=channels)
+
+    def _merge(self, detections: npt.NDArray) -> npt.NDArray:
+        raise NotImplementedError
+
+    def predict(self, cemmndan_channels: int = -1, **parameters):
+        channels = list(self._decompose(cemmndan_channels))
+        channels.insert(0, self.stream)
+        detections = []
+        threads = [Thread(
+            target=detections.append(
+                (idx, OeSNNAD(component, **parameters).predict()))
+        ) for idx, component in enumerate(channels)]
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+
+        detections = [x[1] for x in sorted(detections)]
+        return self._merge(np.array(detections))
---
+++ oesnn_ad/ceemdan/weighted_decompose.py
@@ -0,0 +1,28 @@
+import numpy as np
+import numpy.typing as npt
+from oesnn_ad.ceemdan.decompose import Decompose
+
+
+class WeightedCeemdan(Decompose):
+
+    def __init__(self, stream: npt.NDArray[np.float64],
+                 trials=100,
+                 range_threshold=1e-3,
+                 total_power_threshold=1e-2,
+                 common_ratio=0.7) -> None:
+        super().__init__(stream, trials, range_threshold, total_power_threshold)
+        self.common_ratio = common_ratio
+
+    def _decompose(self, channels: int):
+        return self.ceemdan.ceemdan(self.stream, max_imf=channels)
+
+    def _merge(self, detections: npt.NDArray) -> npt.NDArray:
+        if self.common_ratio < 0.5 or self.common_ratio > 1.0:
+            raise ValueError("Common ratio should be in range [0.5, 1.0]")
+
+        weights = np.array(
+            [self.common_ratio**n for n in range(0, detections.shape[0])])
+        weighted_detections = (
+            detections.T * weights).T
+        summed_columns = weighted_detections.sum(axis=0)
+        return summed_columns > weights.sum() / 2
